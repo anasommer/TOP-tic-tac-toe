b277bb05e8e8c860692b4891f9d0bd234b88a315
Anastassia Sommer <anastassia.sommer@gmail.com>
2024-03-25 16:54:46 +0100

    added some styling

---
+++ main.js
@@ -0,0 +1,47 @@
+const Gameboard = (() => {
+  let gameboard = ['', '', '', '', '', '', '', '', ''];
+
+  const render = () => {
+    let boardHTML = '';
+    gameboard.forEach((square, index) => {
+      boardHTML += `<div class="square" id="square-${index}">${square}</div>`;
+    });
+    document.querySelector('#gameboard').innerHTML = boardHTML;
+  };
+
+  return {
+    render,
+  };
+})();
+
+const createPlayer = (name, mark) => {
+  return {
+    name,
+    mark,
+  };
+};
+
+const Game = (() => {
+  let players = [];
+  let currentPlayerIndex;
+  let gameOver;
+
+  const start = () => {
+    players = [
+      createPlayer(document.querySelector('#player1').value, 'X'),
+      createPlayer(document.querySelector('#player2').value, '0'),
+    ];
+    currentPlayerIndex = 0;
+    gameOver = false;
+    Gameboard.render();
+  };
+
+  return {
+    start,
+  };
+})();
+
+const startBtn = document.querySelector('#start-btn');
+startBtn.addEventListener('click', () => {
+  Game.start();
+});
--- plan.js
+++
@@ -1,2 +0,0 @@
-// Create a gameboard as an array inside of a Gameboard object
-// 2 Players, that are also stored as objects
